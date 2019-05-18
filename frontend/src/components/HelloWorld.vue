07a01df9f2a1cb762bdcd580048c970ea4d904c6
tetsu5555 <tetete1118@gmail.com>
2019-05-18 12:00:00 +0900

    feat: update HelloWorld.vue to add Chat functionality

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,58 +1,47 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h2>RealTimeChat</h2>
+    <!-- 投稿フォーム -->
+    <form @submit.prevent="sendMessage">
+      名前: <input v-model="name" type="text" />
+      投稿内容: <input v-model="message" type="text" />
+      <button type="submit">送信</button>
+    </form>
+    <!-- チャットの表示 -->
+    <div v-for="(row,index) in messages" :key="index">{{ row.name }}: {{ row.message }}</div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  import io from 'socket.io-client';
+
+  export default {
+    name: 'Index',
+    data: () => ({
+      name: '',
+      message: '',
+      messages: []
+    }),
+    methods: {
+      // チャットを投稿する処理
+      sendMessage(e) {
+        e.preventDefault();
+        io('localhost:3000').emit('POST_MESSAGE', {
+            name: this.name,
+            message: this.message
+        });
+        this.message = ''
+      },
+    },
+    mounted(){
+      // 投稿されたデータの取得
+      io('localhost:3000').on('MESSAGE', (data) => {
+        this.messages = [...this.messages, data];
+      })
+    },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
+
